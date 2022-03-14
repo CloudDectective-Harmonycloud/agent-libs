9d3c5e17ccfb053a8c933348c4e10fadb2f7d754
Daxin Wang <idxwang@outlook.com>
2022-03-14 22:15:30 +0800

    add log and remove exception

--- userspace/libsinsp/parsers.cpp
+++ userspace/libsinsp/parsers.cpp
@@ -4906,6 +4906,8 @@ void sinsp_parser::parse_cpu_hotplug_enter(sinsp_evt *evt)
 {
 	if(m_inspector->is_live())
 	{
+		printf("CPU %s configuration change detected. Aborting.",
+		       evt->get_param_value_str("cpu").c_str());
 		throw sinsp_exception("CPU " + evt->get_param_value_str("cpu") +
 				      " configuration change detected. Aborting.");
 	}
